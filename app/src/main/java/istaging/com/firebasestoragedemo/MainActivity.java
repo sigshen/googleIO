0c3176bbbec097682f41b21e2363fa8536c2f57e
sig <sig@staging.com.tw>
2016-06-23 13:10:27 +0800

    Fetch image from gallery

--- app/src/main/java/istaging/com/firebasestoragedemo/MainActivity.java
+++ app/src/main/java/istaging/com/firebasestoragedemo/MainActivity.java
@@ -1,9 +1,10 @@
 package istaging.com.firebasestoragedemo;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.annotation.AnimatorRes;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
+import android.database.Cursor;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     final String TAG = "MainActivity";
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    public int RESULT_LOAD_IMAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,11 +68,14 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+
         Button button = (Button) findViewById(R.id.upload_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                upload();
+                // upload();
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
             }
         });
     }
@@ -126,14 +132,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void upload() {
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            upload(picturePath);
+        }
+    }
+
+    private void upload(String picturePath) {
+        Log.d(TAG, "---picturePath---");
+        Log.d(TAG, picturePath);
         FirebaseStorage storage = FirebaseStorage.getInstance();
 
         // Create a storage reference from our app
         StorageReference storageRef = storage.getReferenceFromUrl("gs://project-508672422003711190.appspot.com");
 
         UploadTask uploadTask;
-        File uploadFile = new File(Environment.getExternalStorageDirectory() + "/iStaging/FolderList/Sample1/HighQuality/Sample1_Outdoors_14645743270_Raw.vr.jpg");
+        // File uploadFile = new File(Environment.getExternalStorageDirectory() + "/iStaging/FolderList/Sample1/HighQuality/Sample1_Outdoors_14645743270_Raw.vr.jpg");
+        File uploadFile = new File(picturePath);
 
         if (uploadFile.exists()) {
             Log.d(TAG, "file exist");
